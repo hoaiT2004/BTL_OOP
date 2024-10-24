1d803bd0e5434df1dbbb5475dc73f6700acc83d1
Nguyễn Xuân Hòa <Nguyenxuanhoa8b@gmail.com>
2024-10-24 09:15:08 +0700

    Init project

---
+++ src/main/java/com/example/btl_oop/BtlOopApplication.java
@@ -0,0 +1,13 @@
+package com.example.btl_oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlOopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtlOopApplication.class, args);
+    }
+
+}
