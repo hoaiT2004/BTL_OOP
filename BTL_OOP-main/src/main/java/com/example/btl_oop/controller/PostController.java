2f52403c96c2d9eaf99a1bb183db13f9bf5492f1
PhatLe <phatletuan153@gmail.com>
2024-11-05 13:48:45 +0700

    update

--- BTL_OOP-main/src/main/java/com/example/btl_oop/controller/PostController.java
+++ BTL_OOP-main/src/main/java/com/example/btl_oop/controller/PostController.java
@@ -1,12 +1,19 @@
 package com.example.btl_oop.controller;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/api/home")
 public class PostController {
 
+    @GetMapping("/nhadephon")
+    public String showHouseBeautyPage() {
+        return "nhadephon"; // Tên của file HTML (không cần phần mở rộng .html)
+    }
+    @GetMapping("/nenthuenhaodau")
+    public String showRentalAdvicePage() {
+        return "nenthuenhaodau"; // Trả về tên file HTML (không cần phần mở rộng .html)
+    }
 }
